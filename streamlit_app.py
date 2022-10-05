3920cc57be8db2b1651ad5bd75bcd6e3139f37ff
shubham shinde <shindeshubham080@gmail.com>
2022-10-05 22:31:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,1 @@
-import streamlit
-
-streamlit.title('My parents new healthy diner')
-
-streamlit.header('🥣   Breakfast Menu')
-streamlit.text('🐔Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
